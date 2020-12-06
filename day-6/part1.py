e8e152bb653cff37be1e9d9509c366cb40a1d203
himohimo <himohimo@gmail.com>
2020-12-06 21:18:07 +0000

    Adds day 6 part 1

---
+++ day-6/part1.py
@@ -0,0 +1,28 @@
+def readLines(fileName):
+
+  with open(fileName) as f:
+    return [line.rstrip() for line in f.readlines()]
+
+def main():
+  import sys
+
+  totalCount = 0
+  currentGroupCount = set()
+
+  lines = readLines('input_test')
+  for l in lines:
+    
+    if l == '':
+      totalCount += len(currentGroupCount)
+      currentGroupCount = set()
+    else:
+      for c in l:
+        currentGroupCount.add(c)
+
+  totalCount += len(currentGroupCount)
+
+  sys.stdout.write(str(totalCount) + '\n')
+  return totalCount
+
+if __name__ == '__main__':
+  main()
